0d76ca623dd2ced4b58924f620dade670e1b3a10
mike2git <seror@abes.fr>
2023-11-03 16:00:00 +0100

    Change command to user@host:mount_path

--- sshm.sh
+++ sshm.sh
@@ -4,10 +4,10 @@
 # default settings
 MOUNT_OPTIONS=
 MOUNT_POINT=
+BASE_MOUNT_POINT="/home/seror/sshfs"
 MOUNT_PATH=
-MOUNT_USER=
+MOUNT_USER_AT_HOST=
 MOUNT_PASSWORD=
-MOUNT_HOST=
 MOUNT_COMMAND=mount
 
 # array to hold arguments without options
@@ -19,18 +19,22 @@ PACKAGE=`basename $0`
 function Usage()
 {
     cat <<-ENDOFMESSAGE
-$PACKAGE - mount filesystem over ssh
-$PACKAGE [command] [mount point] [mount path] [options]
+$PACKAGE - mount or umount filesystem over ssh on /home/seror/user@host
+$PACKAGE [command] [user@host[:mount_path]] [options]
 arguments:
 command - the command to execute, mount / m or unmount / u
-mount point - the path to the mount point
-mount path - the path on the remote server to mount
+user@host:mount_path - the user, the host and the path on the remote server to mount
+
 options:
 -h, --help show brief help
--u, --user MOUNT_USER the username for the ssh login
--H, --host MOUNT_HOST the hostname for the ssh login
+-m, --mountpoint MOUNT_POINT the local mount point
 -p, --password MOUNT_PASSWORD the password for the ssh login
 -o, --options MOUNT_OPTIONS the options string to pass to fusermount
+
+Examples :
+$PACKAGE m r_pse@cbs_app_dev:/home/r_pse
+$PACKAGE u r_pse@cbs_app_dev
+
 NOTE: 
 This command requires the fuse-sshfs package.
 The 'allow_other' option string value will require the /etc/fuse.conf option of 'user_allow_other'.
@@ -55,18 +59,11 @@ function ProcessArguments() {
         opt=$1
         shift
         case ${opt} in
-            -u|--user)
+            -m|--mountpoint)
                 if [ $# -eq 0 -o "${1:0:1}" = "-" ]; then
                     Die "The ${opt} option requires an argument."
                 fi
-                export MOUNT_USER=$1
-                shift
-                ;;
-            -H|--host)
-                if [ $# -eq 0 -o "${1:0:1}" = "-" ]; then
-                    Die "The ${opt} option requires an argument."
-                fi
-                export MOUNT_HOST=$1
+                export MOUNT_POINT=$1
                 shift
                 ;;
             -p|--password)
@@ -98,17 +95,18 @@ function ProcessArguments() {
                 ARGV+=(${opt});;
         esac
     done
-
-    if [ ${#ARGV[@]} -gt 0 ]; then
-        export MOUNT_COMMAND=${ARGV[0]}
-    fi
-
-    if [ ${#ARGV[@]} -gt 1 ]; then
-        export MOUNT_POINT=${ARGV[1]}
-    fi
-
-    if [ ${#ARGV[@]} -gt 2 ]; then
-        export MOUNT_PATH=${ARGV[2]}
+    
+    export MOUNT_COMMAND=${ARGV[0]}
+    if [[ ${ARGV[1]} == *:* ]]; then
+        argument1=${ARGV[1]}
+        export MOUNT_USER_AT_HOST=${argument1%%:*}
+        export MOUNT_PATH=${argument1#*:}
+        export MOUNT_POINT="$BASE_MOUNT_POINT/${MOUNT_USER_AT_HOST}"
+        echo "variable : $argument1"
+        echo "Mount_user_at_host : $MOUNT_USER_AT_HOST"
+        echo "Mount_path : $MOUNT_PATH"
+    else
+    export MOUNT_POINT="$BASE_MOUNT_POINT/${ARGV[1]}"
     fi
 }
 
@@ -120,9 +118,9 @@ ProcessArguments $*
 case ${MOUNT_COMMAND} in
     m|mount)
         if [ -z "$MOUNT_OPTIONS" ]; then
-            echo ${MOUNT_PASSWORD} | sshfs ${MOUNT_USER}@${MOUNT_HOST}:${MOUNT_PATH} ${MOUNT_POINT} -o password_stdin
+            echo ${MOUNT_PASSWORD} | sshfs ${MOUNT_USER_AT_HOST}:${MOUNT_PATH} ${MOUNT_POINT} -o password_stdin
         else
-            echo ${MOUNT_PASSWORD} | sshfs ${MOUNT_USER}@${MOUNT_HOST}:${MOUNT_PATH} ${MOUNT_POINT} -o password_stdin,${MOUNT_OPTIONS}
+            echo ${MOUNT_PASSWORD} | sshfs ${MOUNT_USER_AT_HOST}:${MOUNT_PATH} ${MOUNT_POINT} -o password_stdin,${MOUNT_OPTIONS}
         fi
         ;;
     u|unmount)
